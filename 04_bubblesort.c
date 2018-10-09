751df40d11cd3af5e4fbd331b6069c7d799cf8b2
ceccs17d12 <43026043+ceccs17d12@users.noreply.github.com>
2018-10-09 15:30:51 +0530

    no error

---
+++ 04_bubblesort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int bubbleSort(int array[], int size) {
+	int i, j, temp;
+	for (i=size; i>=0; i--) {
+		for (j=0; j<i; j++) {
+			if (array[j] > array[j+1]) {
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
+}
+int main() {
+	int i, j, array[100], size, temp;
+
+	printf("Enter number of elements in the array : ");
+	scanf("%d", &size);
+
+	printf("Enter the array\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
+	}
+
+	bubbleSort(array, size);
+
+	printf("Sorted array \n");
+	for (i=0; i<size; i++) {
+		printf("%d\n", array[i]);
+	}
+}
