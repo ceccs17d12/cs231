d21b6e94e8db5777c65a98d01b81ad05764b1a50
ceccs17d12 <43026043+ceccs17d12@users.noreply.github.com>
2018-10-09 15:54:39 +0530

    recursion not used

---
+++ 05_MergeSortNoRecursion.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+int main()
+{
+	int arr[100],temp[100],i,j,k,n,size,l1,h1,l2,h2;
+
+	printf("Enter the number of elements : ");
+	scanf("%d",&n);
+
+	for(i=0;i<n;i++)
+	{
+		printf("Enter element %d : ",i+1);
+		scanf("%d",&arr[i]);
+	}
+
+	printf("Unsorted list is : ");
+	for( i = 0 ; i<n ; i++)
+		printf("%d ", arr[i]);
+	for(size=1; size < n; size=size*2 )
+	{
+		l1=0;
+		k=0;
+		while( l1+size < n)
+		{
+			h1=l1+size-1;
+			l2=h1+1;
+			h2=l2+size-1;
+			if( h2>=n ) 
+				h2=n-1;
+			
+			i=l1;
+			j=l2;
+			
+			while(i<=h1 && j<=h2 )
+			{
+				if( arr[i] <= arr[j] )
+					temp[k++]=arr[i++];
+				else
+					temp[k++]=arr[j++];
+			}
+			
+			while(i<=h1)
+				temp[k++]=arr[i++];
+			while(j<=h2)
+				temp[k++]=arr[j++];
+			l1=h2+1; 
+		}
+		for(i=l1; k<n; i++) 
+			temp[k++]=arr[i];
+
+		for(i=0;i<n;i++)
+			arr[i]=temp[i];
+		printf("\nSize= %d \nElements are : ",size);
+		for( i = 0 ; i<n ; i++)
+			printf("%d ", arr[i]);
+		
+	}
+	printf("\n Sorted list is :\n");
+	for( i = 0 ; i<n ; i++)
+		printf("%d ", arr[i]);
+	
+	printf("\n");
+	
+	return 0;
+}
+
