59fd181a5ba31239c4d1e8e84a43cf5dcc1e904e
ceccs17d12 <43026043+ceccs17d12@users.noreply.github.com>
2018-10-09 15:23:47 +0530

    Added Function

---
+++ 01_sequentialsearch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int sequentialSearch(int array[], int size, int element) {
+	int i;
+
+	for (i=0; i<size; i++) {
+		if (array[i] == element) {
+			return (i+1);
+		}
+	}
+	return (0);
+}
+int main() {
+	int i=0, found=0, array[100], size, element;
+
+	printf("Enter number of elements : ");
+	scanf("%d", &size);
+
+	printf("Enter elements\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
+	}
+
+	printf("Enter element to search : ");
+	scanf("%d", &element);
+
+	found = sequentialSearch(array, size, element);
+
+	if (found) {
+		printf("Element found at position %d\n", found);
+	} else {
+		printf("Element not found\n");
+	}
+}
