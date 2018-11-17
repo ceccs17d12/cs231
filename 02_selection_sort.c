9078c6e21a98f4fa1eac1b2d88d3ed3d6810bb50
Arun R <40080953+arunz3@users.noreply.github.com>
2018-11-17 09:55:13 +0530

    Updated

---
+++ 02_selection_sort.c
@@ -0,0 +1,46 @@
+/*
+ * 02
+ * Selection Sort
+ * Name : Arun R
+ * Roll No.: !2
+ * 
+ */
+
+#include <stdio.h>
+
+int selectionSort(int array[], int size) {
+	int i, j, pos, temp;
+	for (i=0; i<size; i++) {
+		pos = i;
+
+		for (j=i; j<size; j++) {
+			if (array[j] < array[pos]) {
+				pos =j;
+			}
+		}
+		if (i != pos) {
+			temp = array[i];
+			array[i] = array[pos];
+			array[pos] = temp;
+		}
+	}
+}
+
+int main() {
+	int i, j, pos, array[100], temp, size;
+
+	printf("Enter number of elements in the array : ");
+	scanf("%d", &size);
+
+	printf("Enter the array\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
+	}
+	
+	selectionSort(array, size);
+
+	printf("Sorted array \n");
+	for (i=0; i<size; i++) {
+		printf("%d\n", array[i]);
+	}
+}
